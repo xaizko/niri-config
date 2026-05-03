7243a980687ce09bcc18332a2352ebb3c2e460ef
xaizko <145681428+xaizko@users.noreply.github.com>
2026-05-03 00:37:14 -0400

    Updated Install script

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
-sudo pacman -S flatpak fish fzf zoxide neovim thefuck superfile wireguard-tools satty grim slurp fd 
+sudo pacman -S flatpak fish fzf zoxide neovim thefuck superfile wireguard-tools satty grim slurp fd go python NetworkManager gcc rustup npm 
+
+sudo systemctl enable --now NetworkManager
+
+mkdir -p ~/setup_apps && cd ~/setup_apps
+git clone https://aur.archlinux.org/paru.git
+cd paru && makepkg -si --noconfirm
+
+paru -S brave-bin pacseek
+
+cd ~/setup_apps/
+cp -r niri-config/nvim/ ~/.config/
+cp -r niri-config/bin/* ~/.local/bin/
+chmod +x ~/.local/bin/*
+
+chsh -s $(which fish)
