776a478a9fb3736c77b6e3fc90ade4dfcfa00587
xaizko <145681428+xaizko@users.noreply.github.com>
2026-05-04 17:21:26 -0400

    Add fish config file copy to setup script
    Copy fish configuration file to the user's config directory.

--- fisher-setup.sh
+++ fisher-setup.sh
@@ -1,3 +1,5 @@
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
+cp ~/setup_apps/niri-config/fish/config.fish ~/.config/fish/config.fish
+
 fisher install PatrickF1/fzf.fish
