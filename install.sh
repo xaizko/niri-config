eacbc58e57bba4cb9a4eafd3c7bbf14ef7027a81
xaizko <145681428+xaizko@users.noreply.github.com>
2026-05-13 17:05:47 -0400

    Add ripgrep and tree-sitter-cli to install script
    Needed to fix nvim config

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ sudo pacman -S --noconfirm \
   flatpak fish fzf zoxide neovim thefuck superfile \
   wireguard-tools satty grim slurp fd go python \
   networkmanager gcc rustup npm rsync fastfetch \
-  unzip zip mpv bat tree nano openssh
+  unzip zip mpv bat tree nano openssh ripgrep \
+  tree-sitter-cli
 
 #Set up Rust
 rustup default stable
